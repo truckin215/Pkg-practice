cad428bad053bcca0a047443cddba6a27efb4739
Connor_Murray <connormurray215@gmail.com>
2019-06-24 22:55:04 -0400

    Ready for class extends

---
+++ index.js
@@ -0,0 +1,22 @@
+const jamesfisher215 = require('jamesfisher215');
+
+
+console.log(jamesfisher215)
+
+class Fisher extends jamesfisher215.james{
+    constructor(attack,defend,dodge){
+        super(attack,defend,dodge)
+        this.Name = 'James Fisher';
+    }
+}
+
+let jake = new Fisher ("hammerfist",'sheild','roll');
+
+// class LittleRobot extends jamesfisher215.helloearth
+
+
+// console.log(jamesfisher215)
+
+// jamesfisher215.helloearth.name
+
+// jamesfisher215.helloWorld();
